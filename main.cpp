52531805db281c3732f48374828b6801d5235beb
igorgs <igor.gs@hotmail.com>
2022-06-22 21:12:58 -0300

    boost-asio tutorial 3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+void print(const boost::system::error_code& /*e*/, boost::asio::steady_timer* t, int* count)
+{
+    if (*count < 5)
+    {
+        std::cout << *count << std::endl;
+        ++(*count);
+
+        t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+        t->async_wait(boost::bind(print,boost::asio::placeholders::error, t, count));
+
+    }
+}
 
 int main(int, char**) 
 {
-    std::cout << "Hello, world!\n";
-    std::cout << __cplusplus;
+    boost::asio::io_context io;
+
+    int count = 0;
+    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(1));
+
+    t.async_wait(boost::bind(print,boost::asio::placeholders::error, &t, &count));
+    
+    
+    io.run();
+
+    return 0;
 }
